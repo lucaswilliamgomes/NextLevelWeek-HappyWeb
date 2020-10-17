43e29721bc9c469d1b3e1210fa7f09a810df0cdd
lucas william <lucaswilliam133@gmail.com>
2020-10-17 20:50:22 -0300

    create dinamic data for page-orphanages

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -6,7 +6,6 @@ var map = L.map('mapid').setView([-27.2185383, -49.6518011], 15);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 // create icon
-
 const icon = L.icon({
     iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
@@ -14,14 +13,32 @@ const icon = L.icon({
     popupAnchor: [170, 2]
 })
 
-// Create popup overlay
 
-const popup = L.popup ({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('lar das meninas <a href="/orphanage" class="choose-orphanage" > <img src="/images/arrow-white.svg" /> </a>')
+function addMarker({id, name, lat, lng}){
+    
+    // Create popup overlay
+    const popup = L.popup ({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(` ${name} <a href="/orphanage?id=${id}" class="choose-orphanage" > <img src="/images/arrow-white.svg" /> </a>`)
 
-L.marker([-27.2185383, -49.6518011], {icon}).addTo(map)
-    .bindPopup(popup);
+    L.marker([lat, lng], {icon}).addTo(map)
+        .bindPopup(popup);
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+.forEach(span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }   
+
+    addMarker(orphanage)
+})
+
+
+
